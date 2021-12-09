bd49d10770a4b52a85e4af3853c2acbced15e7ba
George <gpc91@users.noreply.github.com>
2021-12-09 04:04:54 +0000

    Implemented new Command interface which allows modular commands to be added to a Processor class. This processor class gets added to the compiler/interpreter for use.
    Will try to roll this into a better architecture/design later, but for now we have the following things working:
    
    - White space ignoring
    - Commenting ability (both end of line and inline, e.g. "+++#inline comment!#+++ #end of line comments\n")
    
    TODO: end of file ',' behaviour as per: https://en.wikipedia.org/wiki/Brainfuck#End-of-file_behavior
    TODO: separate the commands into their own classes

--- BrnFkFramework/Program.cs
+++ BrnFkFramework/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 namespace BrnFkFramework
 {
-    internal class Program
+    internal class  Program
     {
         public static void Main(string[] args)
         {
             string file = File.ReadAllText("test.b");
             Console.WriteLine(file);
-            new Compiler(file, new Data()).Run();
+            new Compiler(file, new Data()).RegisterProcessor().Run();
+            
             //new Compiler("+>+>+>+<+<+<+>+>+>+", new Data(4)).Run();
             //new Compiler("+++>++++[-<+>]<.", new Data()).Run();
         }
@@ -18,11 +22,23 @@ namespace BrnFkFramework
     internal class Compiler
     {
         private int ip = 0; // instruction pointer
+        internal int InstructionPointer{
+            get { return ip; }
+            set { ip = value; }
+        }
         private Data data;
+        internal Data Memory => data;
 
+        // TODO: Maybe seperate the source and it's accessors? 
         private string input;
+        internal string Input => input;
+
+        internal char CurrentCharacter => input[ip];
+        // It is important to check if the next character is not out of bound of the source. If it is, return white space (for now).
+        internal char NextCharacter => (ip >= input.Length-1) ? ' ' : input[ip + 1];
 
         private bool isEof => ip >= input.Length;
+        internal bool IsEOF => isEof;
         
         public Compiler(string input, Data data)
         {
@@ -30,100 +46,15 @@ namespace BrnFkFramework
             this.input = input;
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns>return the exit point</returns>
-        private int Consume(int entry_point = 0)
+        internal void Consume()
         {
-            switch (input[ip])
-            {
-                case '.' : 
-                    // force byte
-                    if (ip < input.Length-1 && input[ip + 1] == '!')
-                    {
-                        Console.WriteLine($"[\x1b[33m'.'\x1b[0m, {ip}]\t[\x1b[36m{data.Pointer}\x1b[0m] {data.Value} ::{data.Value}");
-                        ip++;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"[\x1b[33m'.'\x1b[0m, {ip}]\t[\x1b[36m{data.Pointer}\x1b[0m] {data.Value} ::'{(char)data.Value}'");
-                    }
-                    //Console.WriteLine(data.Value);
-                    break;
-                case ',' :
-                    //Console.WriteLine($"[\x1b[33m','\x1b[0m, {ip}]\tinput.");
-                    data.Value = byte.Parse(Console.ReadLine());
-                    break;
-                case '+' :
-                    Console.WriteLine($"[\x1b[33m'+'\x1b[0m, {ip}]\t[\x1b[36m{data.Pointer}\x1b[0m] {data.Value} -> {data.Value + 1}");
-                    data.Add();
-                    break;
-                case '-' :
-                    //Console.WriteLine($"[\x1b[33m'-'\x1b[0m, {ip}]\t[\x1b[36m{data.Pointer}\x1b[0m] {data.Value} -> {data.Value - 1}");
-                    data.Sub();
-                    break;
-                case '>' :
-                    //Console.WriteLine($"[\x1b[33m'>'\x1b[0m, {ip}]\t[\x1b[36m{data.Pointer}\x1b[0m] -> [{data.Pointer + 1}]");
-                    data.Right();
-                    break;
-                case '<' :
-                    //Console.WriteLine($"[\x1b[33m'<'\x1b[0m, {ip}]\t[\x1b[36m{data.Pointer}\x1b[0m] -> [{data.Pointer - 1}]");
-                    data.Left();
-                    break;
-                case '[' :
-                    //Console.WriteLine($"[\x1b[33m'['\x1b[0m, {ip}]\t");
-                    if (data.Value > 0)
-                    {
-                        //Console.WriteLine($"data value is {data.Value}... entering loop...");
-                        int ep = ip; // entry point of loop
-                        while (!isEof)
-                        {
-                            ip++;
-                            ip = Consume(ep);
-                            
-                            if (!isEof && input[ip] == ']')
-                            {
-                                if (data.Value > 0)
-                                {
-                                    //Console.WriteLine($"setting ip {ip} to {entry_point}");
-                                    ip = entry_point;
-                                }
-                                else
-                                {
-                                    return ip++;
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        //Console.WriteLine("Data value is 0, no loop.");
-                        while (input[ip] != ']')
-                        {
-                            //Console.WriteLine($"Seeking ']' character. Current: {input[ip]}");
-                            ip++;
-                        }
-                    }
-                    break;
-                case ']' :
-                    //Console.WriteLine($"[\x1b[33m']'\x1b[0m, {ip}]\tM");
-                    if (data.Value > 0)
-                    {
-                        //Console.WriteLine($"data value greater {ip} :: {entry_point}");
-                        ip = entry_point;
-                    }
-                    else
-                    { 
-                        //Console.WriteLine($"exiting with return of {ip}");
-                        return ip++;
-                    }
-                    break;
-                case '#':
-                    // implement comments
-                    break;
-            }
-            return ip++;
+            Processor.Process(input[ip]);
+        }
+
+        public Compiler RegisterProcessor()
+        {
+            Processor.Compiler(this);
+            return this;
         }
         
         public void Run()
@@ -136,11 +67,194 @@ namespace BrnFkFramework
 
     }
 
+    internal static class Processor
+    {
+        private static Compiler _compiler;
+        
+        private static Dictionary<char, ICommand> Commands = new Dictionary<char, ICommand>()
+        {
+            {'.', new PrintCommand()},
+            {'^', new TestCommand()},
+            {',', new ReadCommand()},
+            {'+', new MemoryAddCommand()},
+            {'-', new MemorySubtractCommand()},
+            {'>', new MemoryShiftRightCommand()},
+            {'<', new MemoryShiftLeftCommand()},
+            {'[', new ConditionalLeftCommand()},
+            {']', new ConditionalRightCommand()},
+            {' ', new WhitespaceCommand()},
+            {'#', new CommentCommand()}
+        };
+        
+        public static void Process(char input)
+        {
+            Commands[input]?.Execute(_compiler);
+            _compiler.InstructionPointer++;
+        }
+
+        public static void Compiler(Compiler compiler)
+        {
+            _compiler = compiler;
+        }
+        
+        
+    }
+
+    internal interface ICommand
+    {
+        public void Execute(Compiler compiler);
+    }
+
+    internal class PrintCommand : ICommand
+    {
+        public void Execute(Compiler compiler)
+        {
+            //if (compiler.InstructionPointer < compiler.Input.Length-1)
+            {
+                switch (compiler.NextCharacter)
+                {
+                    case '!' :                      // force byte
+                        Console.WriteLine($"{compiler.Memory.Value}");
+                        compiler.InstructionPointer++;
+                        return;
+                    case '@' :                      // force byte debug
+                        Console.WriteLine($"{(char)compiler.Memory.Value} ({compiler.Memory.Value})");
+                        compiler.InstructionPointer++;
+                        return;
+                    default :
+                        Console.Write($"{(char)compiler.Memory.Value}");
+                        //Console.WriteLine($"[\x1b[33m'.'\x1b[0m, {ip}]\t[\x1b[36m{data.Pointer}\x1b[0m] {data.Value} ::'{(char)data.Value}'");
+                        return;
+                }
+                return;
+                //Console.WriteLine($"[\x1b[33m'.'\x1b[0m, {ip}]\t[\x1b[36m{data.Pointer}\x1b[0m] {data.Value} ::{data.Value}");
+            }
+        }
+    }
+
+    internal class ReadCommand : ICommand
+    {
+        public void Execute(Compiler compiler)
+        {
+            compiler.Memory.Value = byte.Parse(Console.ReadLine());
+        }
+    }
+
+    internal class WhitespaceCommand : ICommand
+    {
+        public void Execute(Compiler compiler)
+        {
+            while (!compiler.IsEOF)
+            {
+                if (compiler.NextCharacter != ' ')
+                {
+                    return;
+                }
+                else
+                {
+                    compiler.InstructionPointer++;
+                }
+            }
+        }
+    }
+
+    internal class CommentCommand : ICommand
+    {
+        public void Execute(Compiler compiler)
+        {
+            while (!compiler.IsEOF)
+            {
+                if (compiler.NextCharacter == '#' || compiler.NextCharacter == '\n')
+                {
+                    compiler.InstructionPointer++;
+                    return;
+                }
+                else
+                {
+                    compiler.InstructionPointer++;
+                }
+            }
+        }
+    }
+
+    internal class MemoryAddCommand : ICommand
+    {
+        public void Execute(Compiler compiler)
+        {
+            compiler.Memory.Add();
+        }
+    }
+
+    internal class MemorySubtractCommand : ICommand
+    {
+        public void Execute(Compiler compiler)
+        {
+            compiler.Memory.Sub();
+        }
+    }
+
+    internal class MemoryShiftRightCommand : ICommand
+    {
+        public void Execute(Compiler compiler)
+        {
+            compiler.Memory.Right();
+        }
+    }
+
+    internal class MemoryShiftLeftCommand : ICommand
+    {
+        public void Execute(Compiler compiler)
+        {
+            compiler.Memory.Left();
+        }
+    }
+    
+    internal class ConditionalLeftCommand : ICommand
+    {
+        public void Execute(Compiler compiler)
+        {
+            int entry = ++compiler.InstructionPointer;
+            while (!compiler.IsEOF)
+            {
+                compiler.Consume();
+                if (compiler.CurrentCharacter == ']')
+                {
+                    if (compiler.Memory.Value > 0)
+                    {
+                        compiler.InstructionPointer = entry;
+                    }
+                    else
+                    {
+                        // we now need to break out of the loop as the pointer needs to be advanced by the processor
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    
+    internal class ConditionalRightCommand : ICommand
+    {
+        public void Execute(Compiler compiler)
+        {
+            return;
+        }
+    }
+
+    internal class TestCommand : ICommand
+    {
+        public void Execute(Compiler compiler)
+        {
+            Console.WriteLine("This is a test command!");
+        }
+    }
+    
+    
     internal class Data
     {
         private byte[] data;
 
-        public byte[] Contents => data;
+        private byte[] Block => data;
         
         public byte Value
         {
@@ -156,8 +270,8 @@ namespace BrnFkFramework
             data = new byte[memorySize];
         }
         
-        public byte Add() => Contents[Pointer]++;
-        public byte Sub() => Contents[Pointer]--;
+        public byte Add() => Block[Pointer]++;
+        public byte Sub() => Block[Pointer]--;
         public void Right() => pointer++;
         public void Left() => pointer--;
         
